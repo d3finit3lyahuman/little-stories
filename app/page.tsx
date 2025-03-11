5de6902ea7a49d55b2777f15901b78cbbc9b83d0
d3finit3lyahuman <o.okereke@rgu.ac.uk>
2025-03-11 18:36:06 +0000

    Created this from a React Template, will tidy up and remove unnecessary files as we go, currently pretty messy

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,20 @@ import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import Link from "next/link";
+
+type Story = {
+  id: number;
+  title: string;
+  author: string;
+  content: string;
+}
 
 export default async function Home() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+    <h1>Hello My People</h1>
+    <Link href="/about">About</Link>
     </>
   );
 }
